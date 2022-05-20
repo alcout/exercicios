073d372ca3e6cac4894bc2f655ecb3960a44b9fe
Ana Lucia da Costa Coutinho <analu_cc@hotmail.com>
2022-05-20 19:14:23 -0300

    atividade prática

--- Python/pythonProject/AtividadePratica.py
+++ Python/pythonProject/AtividadePratica.py
@@ -77,6 +77,110 @@ class Ex1AP:
                 break #vai finalizar o looping saindo do while true
 
 
+    #exercício 3 atividade prática
+    #início da função DimensõesObjeto
+    def DimensoesObjeto(self):
+        while True:
+            try:
+                ValorObjeto = 0
+                AlturaObjeto = int(input('Qual a altura do objeto (em cm):'))
+                LarguraObjeto = int(input('Qual a largura do objeto (em cm):'))
+                ComprimentoObjeto = int(input('Qual o comprimento do objeto (em cm):'))
+                VolumeObjeto = AlturaObjeto * LarguraObjeto * ComprimentoObjeto
+                print('O volume do objeto é (em cm³):{}'.format(VolumeObjeto))
+
+                if (VolumeObjeto <= 1000):
+                    ValorObjeto = 10
+                elif (VolumeObjeto >= 1001) and (VolumeObjeto <= 10000):
+                    ValorObjeto = 20
+                elif (VolumeObjeto >= 10001) and (VolumeObjeto <= 30000):
+                    ValorObjeto = 30
+                elif (VolumeObjeto >= 30001) and (VolumeObjeto <= 100000):
+                    ValorObjeto = 50
+                else:
+                    print('Não aceitamos objeto com dimensões tão grande.')
+                    continue
+            except:
+                print('Você digitou alguma dimensão do objeto com valor não numérico.')
+                print('Por favor, entre com as dimensões desejadas novamente.')
+                continue
+            else:
+                return VolumeObjeto
+    #Fim da função DimensõesObjeto
+
+    #início da função PesoObjeto
+    def PesoObjeto(self):
+        while True:
+            try:
+                multiplicadorObjeto = 0
+                PesoObjeto = float(input('Digite o peso do objeto (em kg):'))
+                if (PesoObjeto <= 0.10):
+                    multiplicadorObjeto = 1
+                elif (PesoObjeto >= 0.11) and (PesoObjeto <= 1):
+                    multiplicadorObjeto = 1.5
+                elif (PesoObjeto >= 1.10) and (PesoObjeto <= 10):
+                    multiplicadorObjeto = 2
+                elif (PesoObjeto >= 10.1) and (PesoObjeto <= 30):
+                    multiplicadorObjeto = 3
+                else:
+                    print('Não aceitamos objetos tão pesados')
+                    continue
+            except:
+                print('Você digitou peso do objeto com valor não numérico.')
+                print('Por favor, entre com o peso desejado novamente.')
+                continue
+            else:
+                return multiplicadorObjeto
+    #Fim da função PesoObjeto
+
+    #início da função RotaObjeto
+    def RotaObjeto(self):
+        while True:
+            try:
+                multiplicadorRota = 0
+                print('Selecione a rota:')
+                print('RS - De Rio de Janeiro até São Paulo')
+                print('SR - De São Paulo até Rio de Janeiro')
+                print('BS - De Brasília até São Paulo')
+                print('SB - De São Paulo até Brasília')
+                print('BR - De Brasília até Rio de Janeiro')
+                print('RB - Rio de Janeiro até Brasília')
+                rota = input()
+                if (rota == 'RS'):
+                    multiplicadorRota = 1
+                elif (rota == 'SR'):
+                    multiplicadorRota = 1
+                elif (rota == 'BS'):
+                    multiplicadorRota = 1.2
+                elif (rota == 'SB'):
+                    multiplicadorRota = 1.2
+                elif (rota == 'BR'):
+                    multiplicadorRota = 1.5
+                elif (rota == 'RB'):
+                    multiplicadorRota = 1.5
+                else:
+                    print('Você digitou uma rota inexistente.')
+                    print('Por favor, entre com a rota desejada novamente.')
+                    continue
+            except:
+                print('Você digitou uma rota inexistente.')
+                print('Por favor, entre com a rota desejada novamente.')
+                continue
+            return multiplicadorRota
+    #Fim da função RotaObjeto
+
+    #início da função EmpresaEntrega
+    def EmpresaEntrega(self):
+        print('Bem Vindo a Companhia de Logística Ana Lúcia da Costa Coutinho RU:3932630 ')
+        VolumeObjeto = self.DimensoesObjeto()
+        multiplicadorObjeto = self.PesoObjeto()
+        multiplicadorRota = self.RotaObjeto()
+        ValorFinal = (VolumeObjeto) * (multiplicadorObjeto) * (multiplicadorRota)
+        print('O total a ser pago é:R$ {:.2f} (dimensões:{:.2f} * peso: {:.2f} * rota: {:.2f})'.format(ValorFinal,VolumeObjeto, multiplicadorObjeto,multiplicadorRota))
+    #fim da função EmpresaEntrega
+    #fim exercício 3
+
+
 
 
 
